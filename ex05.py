b1292ac5270873ba4e5c7aacb8211d90c3145852
Rodrigo Duarte <rodrigo.duarte.dev01@gmail.com>
2026-03-15 20:31:13 -0300

    feat: adiciona exercicio 05
    Resposta O(n^2): o trecho quadratico domina o custo total do algoritmo.

---
+++ ex05.py
@@ -0,0 +1,12 @@
+﻿def imprimir_elementos_e_pares(lista):
+    for i in range(len(lista)):
+        print(lista[i])
+
+    for i in range(len(lista)):
+        for j in range(len(lista)):
+            print(lista[i], lista[j])
+
+
+if __name__ == "__main__":
+    valores = [1, 2, 3]
+    imprimir_elementos_e_pares(valores)
