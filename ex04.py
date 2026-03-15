2eed4993b91886fbed750cc0cacac1d024d6fdf0
Rodrigo Duarte <rodrigo.duarte.dev01@gmail.com>
2026-03-15 20:31:13 -0300

    feat: adiciona exercicio 04
    Resposta O(n^2): dois lacos aninhados testam os pares possiveis da lista.

---
+++ ex04.py
@@ -0,0 +1,10 @@
+﻿def pares_com_soma(lista, alvo):
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            if lista[i] + lista[j] == alvo:
+                print(lista[i], lista[j])
+
+
+if __name__ == "__main__":
+    valores = [1, 2, 3, 4, 5, 6]
+    pares_com_soma(valores, 7)
