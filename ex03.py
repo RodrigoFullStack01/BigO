079b936fa86006a0ab930fac12fb0cadf3906d3e
Rodrigo Duarte <rodrigo.duarte.dev01@gmail.com>
2026-03-15 20:31:13 -0300

    feat: adiciona exercicio 03
    Resposta O(log n): a busca binaria reduz o intervalo pela metade a cada iteracao.

---
+++ ex03.py
@@ -0,0 +1,21 @@
+﻿def busca_binaria(lista, alvo):
+    esquerda = 0
+    direita = len(lista) - 1
+
+    while esquerda <= direita:
+        meio = (esquerda + direita) // 2
+        valor = lista[meio]
+
+        if valor == alvo:
+            return meio
+        if valor < alvo:
+            esquerda = meio + 1
+        else:
+            direita = meio - 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    ordenada = [1, 3, 5, 7, 9, 11]
+    print(busca_binaria(ordenada, 7))
