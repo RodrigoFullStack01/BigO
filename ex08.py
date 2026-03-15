cd9a04a47abd3fe4d99b9f8c9e5bca193a9c260a
Rodrigo Duarte <rodrigo.duarte.dev01@gmail.com>
2026-03-15 20:31:14 -0300

    feat: adiciona exercicio 08
    Resposta O(n^2): bubble sort trabalha com comparacoes em dois lacos aninhados.

---
+++ ex08.py
@@ -0,0 +1,12 @@
+﻿def ordenacao_bolha(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+
+if __name__ == "__main__":
+    valores = [5, 1, 4, 2, 8]
+    ordenacao_bolha(valores)
+    print(valores)
