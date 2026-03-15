4f93c6c17fcf7a2b62dfb8506f798c0f35aff02f
Rodrigo Duarte <rodrigo.duarte.dev01@gmail.com>
2026-03-15 20:31:14 -0300

    feat: adiciona exercicio 09
    Resposta O(n^3): a multiplicacao de matrizes quadradas usa tres lacos aninhados.

---
+++ ex09.py
@@ -0,0 +1,16 @@
+﻿def produto_de_matrizes(a, b, n):
+    c = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                c[i][j] += a[i][k] * b[k][j]
+
+    return c
+
+
+if __name__ == "__main__":
+    a = [[1, 2], [3, 4]]
+    b = [[2, 0], [1, 2]]
+    c = produto_de_matrizes(a, b, 2)
+    print(c)
